f17c7799b574474a58edcffe4d89c479d491d059
Azim <azimarynaliev@icloud.com>
2025-02-15 14:07:31 -0600

    fixed to direct a user to dashboard after sign-in, sign-up

--- src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return <SignIn />;
 }
--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
+  const isSignedUp = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedUp) {
+      router.push("/dashboard");
+    }
+  }, [isSignedUp]);
+
   return <SignUp />;
 }
